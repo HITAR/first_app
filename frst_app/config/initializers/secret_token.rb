c44f9a0826cdddbb52f070ee27c10d3b587ecee0
Master Guo <370654630@qq.com>
2014-03-01 15:50:27 +0800

    Initial commit

---
+++ frst_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FrstApp::Application.config.secret_key_base = '6ac24ac8b432b3b89b3ce3f82770037b5cd13651630e509c64e804da52b4a7a9a97b6c148c2cb107291eafca8f4a93fb0eae48fecfd0f51a43e9ed601658bf75'
---
+++ frst_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FrstApp::Application.config.session_store :cookie_store, key: '_frst_app_session'
